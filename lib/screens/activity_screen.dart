de1041f812d64ac9d43146ff2a8dd9f9b315d019
Skizaat <77202823+Skizaat@users.noreply.github.com>
2022-04-06 19:01:33 +0200

    fix graphics of activity screen if no activity

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -15,60 +15,62 @@ class ActivityScaffold extends StatelessWidget {
   }
 
   Widget displayActivityWidget(BuildContext context) {
-    return SizedBox(
-        width: 300,
-        height: 100,
-        child: FutureBuilder(
-          builder: (ctx, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text('${snapshot.error} occured',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                );
-              } else if (snapshot.hasData) {
-                final data = snapshot.data as List<Activity>;
-                if (data.isEmpty) {
-                  return const Center(
-                    child: Text("Vous n'avez pas d'activité d'enregistrée",
-                    style: TextStyle(fontSize: 18),));
-                } else {
-                  return Scaffold(
-                    body: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      itemCount: data.length,
-                      itemBuilder: (BuildContext context,
-                          int index) {
-                        String name = data[index]
-                            .getNameActivity();
-                        String day = data[index].getDay();
-                        int hour = data[index].getHour();
-                        return SingleChildScrollView(
-                            child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(context, MaterialPageRoute(
-                                      builder: (context) => OneActivityRoute(currentActivity: data[index])));
-                                },
-                                child: ListTile(
-                                  leading: Icon(Icons.emoji_events),
-                                  title: Text("$name"),
-                                  trailing: Text("le $day à $hour h"),
-                                )
-                            )
-                        );
-                      },
+    return Scaffold(
+      body: SizedBox(
+          width: 300,
+          height: 100,
+          child: FutureBuilder(
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('${snapshot.error} occured',
+                      style: const TextStyle(fontSize: 18),
                     ),
                   );
+                } else if (snapshot.hasData) {
+                  final data = snapshot.data as List<Activity>;
+                  if (data.isEmpty) {
+                    return const Center(
+                        child: Text("Vous n'avez pas d'activité d'enregistrée",
+                          style: TextStyle(fontSize: 18),));
+                  } else {
+                    return Scaffold(
+                      body: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: data.length,
+                        itemBuilder: (BuildContext context,
+                            int index) {
+                          String name = data[index]
+                              .getNameActivity();
+                          String day = data[index].getDay();
+                          int hour = data[index].getHour();
+                          return SingleChildScrollView(
+                              child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                        builder: (context) => OneActivityRoute(currentActivity: data[index])));
+                                  },
+                                  child: ListTile(
+                                    leading: Icon(Icons.emoji_events),
+                                    title: Text("$name"),
+                                    trailing: Text("le $day à $hour h"),
+                                  )
+                              )
+                          );
+                        },
+                      ),
+                    );
+                  }
                 }
               }
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-          future: SQLiteDbProvider.db.getAllActivities(),
-        )
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+            future: SQLiteDbProvider.db.getAllActivities(),
+          )
+      ),
     );
   }
 }
