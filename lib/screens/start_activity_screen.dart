76e80b3533a3f4e63faac68f5c000a4a9f3e4f91
Skizaat <77202823+Skizaat@users.noreply.github.com>
2022-04-06 14:13:58 +0200

    add a form to start one_activity

--- lib/screens/start_activity_screen.dart
+++ lib/screens/start_activity_screen.dart
@@ -1,7 +1,10 @@
 import 'package:adiap/ActivityDatabase.dart';
 import 'package:flutter/material.dart';
 import 'package:adiap/Classes/activity.dart';
+import "package:shared_preferences/shared_preferences.dart";
+import 'package:flutter/services.dart';
 import 'package:adiap/routes.dart';
+import 'package:adiap/create_perso.dart';
 
 
 class StartActivityScaffold extends StatelessWidget {
@@ -11,22 +14,98 @@ class StartActivityScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20),
-      height: 100,
-      width: 200,
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.black, width: 1)
-      ),
-      child: Column(
-          children: [
-            Text(startingActivity.getNameActivity(), style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-            Text(startingActivity.getDay(), style: const TextStyle(fontSize: 18),),
-            Text(startingActivity.getHour().toString() + ' h', style: const TextStyle(fontSize: 18),),
-          ]
-      ),
+    return Scaffold(
+      resizeToAvoidBottomInset : false,
+        body: Column(
+              children: [
+                Text(startingActivity.getNameActivity(), style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                Text(startingActivity.getDay(), style: const TextStyle(fontSize: 18),),
+                Text(startingActivity.getHour().toString() + ' h', style: const TextStyle(fontSize: 18),),
+                Container(
+                  height: 150,
+                  width: 300,
+                  alignment: Alignment.bottomCenter,
+                  child: Text("Veuillez renseigner les informations suivantes pour qu'ADIAP vous conseille dans l'adaptation de l'insuline", style: TextStyle(fontSize: 19),),
+                ),
+                const StartingForm(),
+              ]
+        ),
+    );
+
+  }
+}
+
+class StartingForm extends StatefulWidget {
+  const StartingForm({Key? key}) : super(key: key);
+
+  @override
+  StartingFormState createState() {
+    return StartingFormState();
+  }
+}
+
+class StartingFormState extends State<StartingForm> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context){
+    int? aiming;
+    return Form(
+        key: _formKey,
+          child: Flex(
+          direction: Axis.vertical,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+                Text("Entrez la valeur de Glycémie que vous souhaitez atteindre au début de votre séance de sport (par défaut la valeur est de 170 UNITE"),
+                TextFormField(
+                onSaved: (value){aiming=value as int;},
+                decoration: const InputDecoration(labelText: "Glycémie visée"),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ], // Only numbers can be entered
+                validator: (value) {
+                  return null;
+                },
+              ),
+            TextFormField(
+              onSaved: (value){aiming=value as int;},
+              decoration: const InputDecoration(labelText: "Rentrez  votre glycémie actuelle"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ], // Only numbers can be entered
+              validator: (value) {
+                return null;
+              },
+            ),
+            Text("Dans combien d'heures commencez vous votre activité physique ?"),
+            TextFormField(
+              onSaved: (value){aiming=value as int;},
+              decoration: const InputDecoration(labelText: "Heures avant l'activité"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ], // Only numbers can be entered
+              validator: (value) {
+                return null;
+              },
+            ),
+            Text("Combien d'heures séparent votre activité physique et votre repas le plus proche ?"),
+            TextFormField(
+              onSaved: (value){aiming=value as int;},
+              decoration: const InputDecoration(labelText: "Repas le plus proche"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ], // Only numbers can be entered
+              validator: (value) {
+                return null;
+              },
+            ),
+          ],
+        )
     );
   }
 }
