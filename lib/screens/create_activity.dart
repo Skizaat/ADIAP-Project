9101a2d8b4e5b5f2913ba601b8b9febf897cd5b9
Valentine DAMOURETTE <valentine.damourette@telecom-sudparis.eu>
2022-05-03 18:52:37 +0200

    modification du form d'activités + propre

--- lib/screens/create_activity.dart
+++ lib/screens/create_activity.dart
@@ -13,8 +13,18 @@ class CreateActivityScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CreateForm(),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Créer une activité'),
+        ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 0, 10, 20),
+          color:  const Color(0xffdaebe5),
+          child: const CreateForm() //Text
+        ), //Container
+      ),
     );
   }
 }
@@ -39,112 +49,121 @@ class CreateFormState extends State<CreateForm> {
   String actIntensite = "Légère";
   String weekday="Lundi";
 
+
   @override
   Widget build(BuildContext context){
     String? typeActivite;
     int? time;
-    return Form(
-      key: _formKey,
-      child: Flex(
+      return Form(
+        key: _formKey,
+        child: Flex(
 
-        direction: Axis.vertical,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget> [
-          TextFormField(
-            controller: activiteController,
-            decoration: const InputDecoration(labelText: "Nom de l'activite"),
-            onSaved: (value) {
-              typeActivite = value;
+          direction: Axis.vertical,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            TextFormField(
+              textAlign: TextAlign.center,
+              controller: activiteController,
+              decoration: const InputDecoration(
+                labelText: "Nom de l'activite",
+                contentPadding: EdgeInsets.all(5),
+              ),
+              onSaved: (value) {
+                typeActivite = value;
+            },
+          // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Entrez le nom de l'activite";
+              }
+              return null;
+            },
+        ),
+        Text("Entrer l'intenstié de l'activité que vous faites"),
+        DropdownButton<String>(
+          value: actIntensite,
+          icon: const Icon(Icons.arrow_downward),
+          elevation: 16,
+          style: const TextStyle(color: Colors.blue),
+          underline: Container(
+            height: 2,
+            color: Colors.blue,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              actIntensite = newValue!;
+            });
           },
-        // The validator receives the text that the user has entered.
+          items: <String>['Légère','Modérée','Intense']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+
+        DropdownButton<String>(
+          value: weekday,
+          icon: const Icon(Icons.arrow_downward),
+          elevation: 16,
+          style: const TextStyle(color: Colors.blue),
+          underline: Container(
+            height: 2,
+            color: Colors.blue,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              weekday = newValue!;
+            });
+          },
+          items: <String>["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+
+        TextFormField(
+          textAlign: TextAlign.center,
+          controller: timeController,
+          onSaved: (value) {
+            time = value as int;
+          },
+          decoration: const InputDecoration(labelText: "Heure"),
+          keyboardType: TextInputType.number,
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.digitsOnly
+          ],
+          // Only numbers can be entered
           validator: (value) {
             if (value == null || value.isEmpty) {
-              return "Entrez le nom de l'activite";
+              return "Entrez l'heure de l'activite (ex 16 pour 16h)";
             }
             return null;
           },
-      ),
-      Text("Entrer l'intenstié de l'activité que vous faites"),
-      DropdownButton<String>(
-        value: actIntensite,
-        icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        style: const TextStyle(color: Colors.blue),
-        underline: Container(
-          height: 2,
-          color: Colors.blue,
         ),
-        onChanged: (String? newValue) {
-          setState(() {
-            actIntensite = newValue!;
-          });
-        },
-        items: <String>['Légère','Modérée','Intense']
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
-      ),
 
-      DropdownButton<String>(
-        value: weekday,
-        icon: const Icon(Icons.arrow_downward),
-        elevation: 16,
-        style: const TextStyle(color: Colors.blue),
-        underline: Container(
-          height: 2,
-          color: Colors.blue,
+        ElevatedButton(
+
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              Activity newActivity = Activity(idActivity: 1,
+                nameActivity: activiteController.text,
+                intensity: actIntensite,
+                day: weekday,
+                hour: int.parse(timeController.text),);
+              await SQLiteDbProvider.db.insert(newActivity);
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => OneActivityRoute(currentActivity: newActivity,)));
+            }
+          },
+          child: Text('Enregistrer'),
         ),
-        onChanged: (String? newValue) {
-          setState(() {
-            weekday = newValue!;
-          });
-        },
-        items: <String>["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
-      ),
-      TextFormField(
-        controller: timeController,
-        onSaved: (value) {
-          time = value as int;
-        },
-        decoration: const InputDecoration(labelText: "Heure"),
-        keyboardType: TextInputType.number,
-        inputFormatters: <TextInputFormatter>[
-          FilteringTextInputFormatter.digitsOnly
-        ],
-        // Only numbers can be entered
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return "Entrez l'heure de l'activite (ex 16 pour 16h)";
-          }
-          return null;
-        },
-      ),
-      ElevatedButton(
-        onPressed: () async {
-          if (_formKey.currentState!.validate()) {
-            Activity newActivity = Activity(idActivity: 1,
-              nameActivity: activiteController.text,
-              intensity: actIntensite,
-              day: weekday,
-              hour: int.parse(timeController.text),);
-            await SQLiteDbProvider.db.insert(newActivity);
-            Navigator.push(context, MaterialPageRoute(
-                builder: (context) => OneActivityRoute(currentActivity: newActivity,)));
-          }
-        },
-        child: Text('Enregistrer'),
-      ),
-    ],
-    )
-    );
+      ],
+      )
+      );
   }
 }
